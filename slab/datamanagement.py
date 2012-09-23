66a176427d5cac793206da6877b98c94fb537425
phil <phil@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2012-09-23 16:19:37 +0000

    Moved get_script into datamanagement.py updated SlabFile to use get_script() Added SlabFile.save_settings() which will save a python dictionary into an existing group/dataset or if it doesn't exist create a new group

--- slab/datamanagement.py
+++ slab/datamanagement.py
@@ -8,13 +8,28 @@ Created on Fri May 18 16:07:13 2012
 
 import sys
 import h5py
+import inspect
 
 class SlabFile(h5py.File):
     def __init__(self, *args, **kwargs):
         h5py.File.__init__(self, *args, **kwargs)
         #self.attrs["_script"] = open(sys.argv[0], 'r').read()
-        self.attrs["_script"] = open(__file__, 'r').read()
+        self.attrs["_script"] = get_script()
         self.flush()
+        
+    def save_settings(self,dic,group='settings'):
+        if group not in self:
+            self.create_group(group)
+        for k in dic.keys():
+            self[group].attrs[k]=dic[k]
+    
+
+def get_script():
+    """returns currently running script file as a string"""
+    f=open(inspect.stack()[-1][1],'r')
+    s=f.read()
+    f.close()
+    return s  
 
 
 def set_range(h5file, xmin, xmax, ymin=None, ymax=None):
