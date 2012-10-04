74e4e11470e641380317b5a3ce47c0f91466fcf6
phil <phil@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2012-10-04 20:46:21 +0000

    Changed behavior of activate/deactivate so that if all switches are thrown it does them sequentially rather than in parallel, this is important because otherwise not enough current gets to each solenoid and the switches don't switch
    DS

--- slab/instruments/relaybox/RFSwitch.py
+++ slab/instruments/relaybox/RFSwitch.py
@@ -16,11 +16,11 @@ import time
 class RFSwitch(RelayBox):    
 
     def __init__(self,name="",address="",enabled=True, RF_Status="000000"):
-        print "Switch", name, "at address", address, " is initiated!" 
+        #print "Switch", name, "at address", address, " is initiated!" 
         RelayBox.__init__(self,name,address,enabled)
         self.RF_Status=RF_Status
         
-    def activate(self, port=0):    
+    def activate(self, port=0,pulse_width=1):    
         if port >0:
             self.set_relay(port, True)
             self.pulse_relay(7)
@@ -30,15 +30,11 @@ class RFSwitch(RelayBox):
             else:
                 self.RF_Status= self.RF_Status[0:port-1] + '1' + self.RF_Status[port:]
         elif port == 0:
-            for i in range(6):
-                self.set_relay(i+1, True)
-            self.pulse_relay(7)
-            for i in range(6):
-                self.set_relay(i+1, False)
-            self.RF_Status='111111'
+            for ii in range(1,7):
+                self.activate(ii,pulse_width)
       
     
-    def deactivate(self, port=0):
+    def deactivate(self, port=0,pulse_width=1):
         if port>0:
             self.set_relay(port, True)
             self.pulse_relay(8)
@@ -48,27 +44,23 @@ class RFSwitch(RelayBox):
             else:
                 self.RF_Status= self.RF_Status[0:port-1] + '0' + self.RF_Status[port:]
         elif port == 0:
-            for i in range(6):
-                self.set_relay(i+1, True)
-            self.pulse_relay(8)
-            for i in range(6):
-                self.set_relay(i+1, False)
-            self.RF_Status='000000'
+            for ii in range(1,7):
+                self.deactivate(ii,pulse_width)
                 
 if __name__=="__main__":
     RF_1= 'http://192.168.14.20'
-    RF_2='http://192.168.14.21'
+    RF_2= 'http://192.168.14.21'
     
     rfs_1=RFSwitch(name="Switch1", address=RF_1)
     rfs_2=RFSwitch(name="Switch2", address=RF_2)
 
-    for i in range(6):
-       rfs_1.activate(i+1)
-       print rfs_1.RF_Status
-    
-    for i in range(6):
-       rfs_1.deactivate(i+1)
-       print rfs_1.RF_Status
+#    for i in range(6):
+#       rfs_1.activate(i+1)
+#       print rfs_1.RF_Status
+#    
+#    for i in range(6):
+#       rfs_1.deactivate(i+1)
+#       print rfs_1.RF_Status
     
     
     
