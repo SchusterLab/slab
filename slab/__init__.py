4af08b1fe1d78a38115e159196ec624218e7d382
dave <dave@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2012-10-13 20:09:15 +0000

    made circuitqed optional

--- slab/__init__.py
+++ slab/__init__.py
@@ -7,7 +7,8 @@ import gui
 import script
 from analysisScript import *
 from datamanagement import *
-from circuitqed import *
+try: from circuitqed import *
+except: print "Warning: Could not import circuitqed. Probably due to qutip"
 #import diamond
 try: import widgets
 except: print "Warning: Could not import widgets"
