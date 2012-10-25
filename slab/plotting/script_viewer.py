88ce5622518ad96a8585e9f91d854f994452a9c8
phil <phil@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2012-10-25 17:10:24 +0000

    All sorts of fun features! (*bugs waiting to happen*)

--- slab/plotting/script_viewer.py
+++ slab/plotting/script_viewer.py
@@ -10,20 +10,27 @@ Created on Thu Oct 18 10:00:10 2012
 import zmq
 import numpy as np
 
-#sv_addr = "tcp://127.0.0.1:9000"
-#sv_meta_addr = "tcp://127.0.0.1:9001"
-
 class ScriptPlotter(zmq.Context):
     def __init__(self):
         zmq.Context.__init__(self)
         self.pub = self.socket(zmq.PUB)
         self.meta_pub = self.socket(zmq.PUB)
+        self.text_pub = self.socket(zmq.PUB)
         self.pub.bind("tcp://127.0.0.1:5556")
         self.meta_pub.bind("tcp://127.0.0.1:5557")
-        time.sleep(2)
-    def init_plot(self, ident, rank=1, accum=True):
-        self.meta_pub.send_json({'ident':ident, 'rank':rank, 'accum':accum})
-    def send_array(self, A, ident, flags=0, copy=True, track=False):
+        self.text_pub.bind("tcp://127.0.0.1:5558")
+        time.sleep(.5)
+    def init(self, ident, rank=1, accum=True, **kwargs):
+        self.meta_pub.send_json({'ident':ident, 
+                                 'rank':rank,
+                                 'accum':accum,
+                                 'plotkwargs':kwargs})
+        time.sleep(.25)
+    
+    def plot(self, data, ident):
+        self.send_array(np.array(data), ident)
+        
+    def send_array(self, A, ident, flags=0, copy=False, track=False):
         """send a numpy array with metadata"""
         md = dict(
             ident = ident,
@@ -35,6 +42,10 @@ class ScriptPlotter(zmq.Context):
     def close(self):
         self.pub.close()
         self.meta_pub.close()
+        self.text_pub.close()
+        
+    def msg(self, text):
+        self.text_pub.send_unicode(text)
 
 
 import slab.gui as gui
@@ -48,20 +59,27 @@ class ScriptViewerThread(gui.DataThread):
         self.meta_sub = self.ctx.socket(zmq.SUB)
         self.meta_sub.setsockopt(zmq.SUBSCRIBE, "")
         self.meta_sub.connect("tcp://127.0.0.1:5557")
+        self.text_sub = self.ctx.socket(zmq.SUB)
+        self.text_sub.setsockopt(zmq.SUBSCRIBE, "")
+        self.text_sub.connect("tcp://127.0.0.1:5558")
+
         self.msg("polling started")
         while not self.aborted():
-            time.sleep(.5)
+            #time.sleep(.05)
             if self.sub.poll(10):
-                self.msg("data hit")
                 self.recv_array()
             if self.meta_sub.poll(10):
-                self.msg("new plot hit")
                 self.recv_new_plot()
-                
+            if self.text_sub.poll(10):
+                self.msg(self.text_sub.recv_unicode())
         self.msg("polling complete")
+    
     def recv_new_plot(self):
         d = self.meta_sub.recv_json()
-        self.gui["plotstacker"].add_plot(**d)
+        try:
+            self.gui["plotstacker"].add_plot(**d)
+        except Exception as e:
+            self.msg(e)
         
     def recv_array(self, flags=0, copy=True, track=False):
         """recv a numpy array"""
@@ -69,52 +87,131 @@ class ScriptViewerThread(gui.DataThread):
         msg = self.sub.recv(flags=flags, copy=copy, track=track)
         buf = buffer(msg)
         A = np.frombuffer(buf, dtype=md['dtype'])
-        self.gui["plotstacker"].update_plot(md['ident'], A.reshape(md['shape']))
+        try:
+            self.gui["plotstacker"].update_plot(md['ident'], A.reshape(md['shape']))
+        except Exception as e:
+            self.msg(e)
             
 import PyQt4.Qt as qt
 from guiqwt.plot import CurveWidget, ImageWidget
 from guiqwt.builder import make
-class PlotStacker(qt.QWidget):
-    def __init__(self):
+
+class PlotItem(qt.QWidget):
+    def __init__(self, ident, rank, accum, plotkwargs):
         qt.QWidget.__init__(self)
-        self.setLayout(qt.QGridLayout())
+        qt.QVBoxLayout(self)
+        
+        self.ident = ident
+        self.rank = rank
+        self.accum = accum
+        
+        if rank == 1:
+            self.plot_widget = CurveWidget(title=ident)
+            self.item = make.curve([], [], **plotkwargs)
+        elif rank == 2:
+            print plotkwargs
+            self.plot_widget = ImageWidget(title=ident, lock_aspect_ratio=False)
+            self.item = make.image(np.array([[0]]), **plotkwargs)
+        else:
+            raise ValueError
+        self.plot_widget.plot.add_item(self.item)
+        self.layout().addWidget(self.plot_widget)
+
+        buttons = qt.QWidget()
+        qt.QHBoxLayout(buttons)
+        self.remove_button = qt.QPushButton('Remove')
+        self.zoom_button = qt.QPushButton('Zoom')
+        buttons.layout().addWidget(self.remove_button)
+        buttons.layout().addWidget(self.zoom_button)
+        self.layout().addWidget(buttons)
+
+class PlotStacker(qt.QWidget):
+    def __init__(self, mainwin):
+        qt.QWidget.__init__(self)
+        self.mainwin = mainwin
+        #self.setLayout(qt.QVBoxLayout())
+        self.setLayout(qt.QHBoxLayout())
+        plwidget = qt.QWidget()
+        self.plwidget = plwidget
+        self.plotlist = qt.QVBoxLayout(plwidget)
+        #plwidget.setSizePolicy(qt.QSizePolicy.Maximum)
+        plwidget.setMaximumWidth(2 ** 16)
+        self.layout().addWidget(plwidget)
+        zoom_widget = qt.QWidget()
+        self.zoom = qt.QHBoxLayout(zoom_widget)
+        self.layout().addWidget(zoom_widget)
         self.row = 0
         self.col = 0
         self.plots = {}
         self.accum = {}
-    def add_plot(self, ident="", rank=1, accum=True):
-        if rank < 2:
-            widget = CurveWidget(title=ident)
-            item = make.curve([],[])
-        else:
-            widget = ImageWidget(title=ident)
-            item = make.image()
-        widget.plot.add_item(item)
+   
+    def add_plot(self, ident="", rank=1, accum=True, plotkwargs={}):
+        if ident in self.plots:
+            self.remove_plot(ident)
+        widget = PlotItem(ident, rank, accum, plotkwargs)
+        widget.remove_button.clicked.connect(lambda: self.remove_plot(ident))
+        widget.zoom_button.clicked.connect(lambda: self.zoom_plot(ident))
         self.plots[ident] = widget
-        self.layout().addWidget(widget, self.row, self.col)
-        # Two columns
-        self.row += self.col
-        self.col = 1 - self.col
-        self.plots[ident] = \
-          {"widget":widget.plot, "accum":accum, "item":item, "rank":rank}
-    def update_plot(self, ident, data):
-        plot = self.plots[ident]
-        item = plot["item"]
-        if plot["accum"]:
-            if plot["rank"] is 1:
-                x, y = item.get_data()
-                x = np.concatenate((x, [data[0]]))
-                y = np.concatenate((y, [data[1]]))
-                item.set_data(x, y)
-            else:
-                raise NotImplementedError
+        self.plotlist.addWidget(widget)
+    
+    def zoom_plot(self, ident):
+        item = self.zoom.itemAt(0)
+        if item is not None:
+            widget = item.widget()
+            self.zoom.removeWidget(widget)
+            self.plotlist.addWidget(widget)
+            if widget.ident == ident:
+                self.plwidget.setMaximumWidth(2 ** 16)
+                return
         else:
-            if plot["rank"] is 1:
-                x, y = data
+            self.plwidget.setMaximumWidth(2 ** 9)
+        widget = self.plots[ident]
+        self.plotlist.removeWidget(widget)
+        self.zoom.addWidget(widget)
+        
+                             
+    def remove_plot(self, ident):
+        widget = self.plots.pop(ident)
+        if widget.parentWidget.layout() is self.plotlist:
+            self.plotlist.removeWidget(widget)
+        else:
+            self.zoom.removeWidget(widget)
+        widget.close()
+    
+    def update_plot(self, ident, data):
+        try:
+            plot = self.plots[ident]
+        except KeyError:
+            self.add_plot(ident, rank=1 if (len(data) == 2) else 2)
+            plot = self.plots[ident]
+        item = plot.item
+        if plot.accum is True and plot.rank is 1:
+            x, y = item.get_data()
+            x = np.concatenate((x, [data[0]]))
+            y = np.concatenate((y, [data[1]]))
+            item.set_data(x, y)
+        elif plot.accum in (0, 1, True) and plot.rank is 2:
+            img = item.data
+            if plot.accum in (0, True):
+                if img.shape == (1,1): # Yet to be properly initialized
+                    img = np.array([data])
+                else:
+                    img = np.vstack((img, data))
+            else:
+                img = np.column_stack((img, data))
+            item.set_data(img)
+        else:
+            if plot.rank is 1:
+                try:
+                    x, y = data
+                except:
+                    y = data
+                    x = np.arange(len(y))
                 item.set_data(x, y)
             else:
-                raise NotImplementedError
-        plot["widget"].replot()
+                item.set_data(data)
+        plot.plot_widget.plot.replot()
+        plot.plot_widget.plot.do_autoscale()
 
 from guiqwt.qtdesigner import loadui
 UiClass = loadui("C:\_Lib\python\slab\plotting\script_viewer.ui")
@@ -124,12 +221,18 @@ class ScriptViewerWindow(gui.SlabWindow, UiClass):
         gui.SlabWindow.__init__(self, ScriptViewerThread)
         self.setupSlabWindow(autoparam=True)
         #self.auto_register_gui()
-        ps = PlotStacker()
-        self.verticalLayout.addWidget(ps)
+        ps = PlotStacker(self)
+        splitter = qt.QSplitter()
+        self.message_box = qt.QTextBrowser()
+        self.message_box.setMaximumWidth(250)
+        splitter.addWidget(self.message_box)
+        splitter.addWidget(ps)
+        self.verticalLayout.addWidget(splitter)
         self.gui["plotstacker"] = ps
         self.register_script("start_polling", self.start_button, self.stop_button)
         self.start_thread()
         self.msg("initialized")
+        self.start_script("start_polling")
         
 import sys
 import time
@@ -139,15 +242,22 @@ def view():
 
 def serve(n):
     print "Serving test data"
-    p = ScriptPlotter()
-    p.init_plot("sin")
+    plotter = ScriptPlotter()
+    #plotter.init("sin", rank=2)
+    #plotter.init("cos", rank=1, color='r')
+    plotter.init("tan", rank=1, accum=False)
     t = 0
-    print "here"
+    x = np.linspace(0, 2*np.pi)
+    print "starting"
     for i in range(n):
+        time.sleep(.1)
         t += .1
-        p.send_array(np.array([t, np.sin(t)]), "sin")
-        time.sleep(.1)    
-    p.close()
+        plotter.msg("time" + str(t))
+        plotter.plot(np.sin(x + t), "sin")
+        plotter.plot((t, np.cos(t)), "cos")
+        plotter.plot(np.tan(x + t), "tan")
+    plotter.close()
+    print "done"
     
 if __name__ == "__main__":
     view()    
