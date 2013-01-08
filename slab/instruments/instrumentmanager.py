fb74ca685261a6b995c7f261257d7e9488dff827
phil <phil@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-01-08 20:04:46 +0000

    Set Pyro4 servertype in header

--- slab/instruments/instrumentmanager.py
+++ slab/instruments/instrumentmanager.py
@@ -9,21 +9,25 @@ import os
 import sys
 try:
     import Pyro4
-    Pyro4Loaded=True
+    Pyro4Loaded = True
+    # Block calls from running simultaneously
+    Pyro4.config.SERVERTYPE = 'multiplex'
 except:
-    print "Warning: Pyro4 package is not present, Instrument Servers will not work."
-    Pyro4Loaded=False
+    print "Warning: Pyro4 package is not present"
+    print "Instrument Servers will not work."
+    Pyro4Loaded = False
+
 
 class InstrumentManager(dict):
-    """InstrumentManager class reads configuration files and 
+    """InstrumentManager class reads configuration files and
     keeps track of listed instruments and their settings
     """
-    def __init__(self,config_path=None,server=False,ns_address=None):
+    def __init__(self, config_path=None, server=False, ns_address=None):
         """Initializes InstrumentManager using config_path if available"""
         dict.__init__(self)
-        self.config_path=config_path
-        self.config=None
-        self.ns_address=ns_address
+        self.config_path = config_path
+        self.config = None
+        self.ns_address = ns_address
         #self.instruments={}
         if not server and Pyro4Loaded:
                 try:
@@ -35,23 +39,24 @@ class InstrumentManager(dict):
             self.load_config_file(config_path)
         if server and Pyro4Loaded:
                 self.serve_instruments()
-        
-    def load_config_file(self,config_path):
+
+    def load_config_file(self, config_path):
         """Loads configuration file"""
         print "Loaded Instruments: ",
-        f=open(config_path,'r')
+        f = open(config_path, 'r')
         for line in f.readlines():
-            if line[0]!='#' and line[0]!='':
-                name=line.split()[0]
+            if line[0] != '#' and line[0] != '':
+                name = line.split()[0]
                 print name,
-                self[name]=self.load_instrument(line)
+                self[name] = self.load_instrument(line)
         print "!"
-    
-    def load_instrument(self,config_string):
+
+    def load_instrument(self, config_string):
         """Loads instrument based on config_string (Name\tAddress\tType)"""
         #print config_string
-        params=config_string.split()
-        return getattr(slab.instruments,params[1])(name=params[0],address=params[2])
+        params = config_string.split()
+        fn = getattr(slab.instruments, params[1])
+        return fn(name=params[0], address=params[2])
 
     def serve_instruments(self):
         """inst_dict is in form {name:instrument_instance}"""
@@ -61,53 +66,54 @@ class InstrumentManager(dict):
         for name, instrument_instance in self.items():
             uri = daemon.register(instrument_instance)
             ns.register(name, uri)
-            print "Registered: %s\t%s" %(name,uri)
+            print "Registered: %s\t%s" % (name, uri)
         daemon.requestLoop()
-        
+
     def connect_proxies(self):
-        ns=Pyro4.locateNS(self.ns_address)
-        for name,uri in ns.list().items():
-            self[name]=Pyro4.Proxy(uri)
-        
+        ns = Pyro4.locateNS(self.ns_address)
+        for name, uri in ns.list().items():
+            self[name] = Pyro4.Proxy(uri)
+
     def get_settings(self):
         """Get settings from all instruments"""
-        settings=[]
-        for k,inst in self.iteritems():
+        settings = []
+        for k, inst in self.iteritems():
             settings.append(inst.get_settings())
         return settings
-        
-    def save_settings(self,path,prefix=None,params={}):
+
+    def save_settings(self, path, prefix=None, params={}):
         """Get settings from all instruments and save to a .cfg file"""
-        settings=self.get_settings()
+        settings = self.get_settings()
         settings.append(params)
         if prefix:
-            fname=os.path.join(path,prefix)
+            fname = os.path.join(path, prefix)
         else:
             #print "hey"
             fname = path
         if ".cfg" not in fname.lower():
-            fname+='.cfg'
-        f=open(fname,'w')
+            fname += '.cfg'
+        f = open(fname, 'w')
         for s in settings:
             f.write(repr(s))
             f.write('\n')
         f.close()
-        
+
     def clean_nameserver(self):
-        """Checks to make sure all of the names listed in server are really there"""
-        ns=Pyro4.locateNS(self.ns_address)
-        for name,uri in ns.list().items():
-            try:            
+        """Checks to make sure all of the names listed
+        in server are really there"""
+        ns = Pyro4.locateNS(self.ns_address)
+        for name, uri in ns.list().items():
+            try:
                 Pyro4.Proxy(uri).get_id()
             except:
                 ns.remove(name)
-        
-if __name__=="__main__":
-    if len(sys.argv)>1:
-        cp=sys.argv[1]
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        cp = sys.argv[1]
     else:
-        cp=r"S:\_Data\120930 - EonHe - M005CHM3\004_AutomatedFilling\instruments.cfg"
-    
-    im = InstrumentManager(config_path=cp,server=True)
+        cp = r"S:\_Data\120930 - EonHe - M005CHM3\004_AutomatedFilling\instruments.cfg"
+
+    im = InstrumentManager(config_path=cp, server=True)
     #print im['fil'].get_id()
     #im.serve_instruments()
