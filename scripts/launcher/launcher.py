5d21421d2f784a9065c61d86a0256b658d5cf366
phil <phil@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-01-29 22:35:44 +0000

    executable version of launcher.py

--- scripts/launcher/launcher.py
+++ scripts/launcher/launcher.py
@@ -5,7 +5,7 @@ Created on Mon Jan 28 17:33:23 2013
 @author: slab
 """
 
-import PyQt4.Qt as qt
+from PyQt4 import QtGui, QtCore
 from subprocess import Popen
 import os
 
@@ -19,19 +19,18 @@ scripts = {
     'File Server' : [os.path.join(slab_dir, 'datamanagement.py')]
 }
 
-
 def create_popen_fn(args):
     env = os.environ
     env.update({'PYTHONPATH':'C:\\_Lib\\python'})
-    return lambda: Popen(['python'] + args, env=env)
+    return lambda: Popen(['pythonw'] + args, env=env)
 
 if __name__ == "__main__":
-    app = qt.QApplication([])
-    win = qt.QMainWindow()
-    widget = qt.QWidget()
-    layout = qt.QVBoxLayout(widget)
+    app = QtGui.QApplication([])
+    win = QtGui.QMainWindow()
+    widget = QtGui.QWidget()
+    layout = QtGui.QVBoxLayout(widget)
     for name, args in scripts.items():
-        button = qt.QPushButton(name)
+        button = QtGui.QPushButton(name)
         button.clicked.connect(create_popen_fn(args))
         layout.addWidget(button)
     win.setCentralWidget(widget)    
