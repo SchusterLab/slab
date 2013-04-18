1d0d2a766a740a0ab7c4b5aefa49e02533ed21b6
ge <ge@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-04-18 17:45:19 +0000

    intermediate (incomplete) changes. Commit for migrate

--- slab/instruments/Alazar/alazar_parallel.py
+++ slab/instruments/Alazar/alazar_parallel.py
@@ -13,13 +13,13 @@ records_per_buffer = 10
 records_per_acquisition = 0x7fffffff
 bytes_per_sample = 1
 samples_per_record = 2**14
-buffers_per_merge = 1000
+buffers_per_merge = 10
 buffer_count = 8
 iterations = 100000
 timeout = 1000
 samples_per_ms = 1000000
 samples_per_second = samples_per_ms * 1e3
-seconds_per_record = 17e-6
+seconds_per_record = 50e-6
 seconds_per_buffer = seconds_per_record * records_per_buffer
 bytes_per_record = bytes_per_sample * samples_per_record
 bytes_per_buffer = bytes_per_record * records_per_buffer
@@ -41,8 +41,28 @@ def validate_config_parameters(config, trigger_time):
      if duty_cycle > .7:
          raise ValueError('Card is acquiring for %.1f of the trigger time' % duty_cycle)
 
+def average_worker(ignored, buf, buf_ready_event, buf_post_event, avg_buffer, buffers_merged):
+    arr = np.frombuffer(buf.get_obj(), U8)
+    avg_buffer_arr = np.frombuffer(avg_buffer.get_obj(), C.c_longdouble)
+    sum_buffer = np.zeros(bytes_per_buffer, np.uint32)
+    my_buffers_completed = 0
+    for _ in range(iterations):
+        buf_ready_event.wait()
 
-def average_worker(reshape_fn, buf, buf_ready_event, buf_post_event, avg_buffer, buffers_merged):
+        buf_ready_event.clear()
+        sum_buffer += arr
+        buf_post_event.set()
+        my_buffers_completed += 1
+
+        if my_buffers_completed % buffers_per_merge == 0:
+            with avg_buffer.get_lock():
+                n = buffers_merged.value
+                avg_buffer_arr *= (n - 1.) / n
+                avg_buffer_arr += sum_buffer / (n*buffers_per_merge)
+                buffers_merged.value += 1
+            sum_buffer.fill(0)
+
+def average_reshape_worker(reshape_fn, buf, buf_ready_event, buf_post_event, avg_buffer, buffers_merged):
     arr = np.frombuffer(buf.get_obj(), U8)
     avg_buffer_arr = np.frombuffer(avg_buffer.get_obj(), C.c_longdouble)
     sum_buffer = np.zeros(bytes_per_buffer, np.uint32)
@@ -63,6 +83,13 @@ def average_worker(reshape_fn, buf, buf_ready_event, buf_post_event, avg_buffer,
                 buffers_merged.value += 1
             sum_buffer.fill(0)
 
+def reshape_collapse_records(res, arr):
+    samples_per_record = len(res)
+    records_per_buffer = int(len(arr) / samples_per_record)
+    res.fill(0)
+    for i in range(records_per_buffer):
+        res += arr[i*samples_per_record:(i+1)*samples_per_record]
+
 def single_shot_worker(proc_fun, buf, buf_ready_event, buf_post_event, result_buffer, buffers_merged):
     arr = np.frombuffer(buf.get_obj(), U8)
     results = []
@@ -76,11 +103,11 @@ def single_shot_worker(proc_fun, buf, buf_ready_event, buf_post_event, result_bu
 
     with result_buffer.get_lock():
         n = buffers_merged.value
-        result_buffer[n*iterations:(n+1)*iterations] = array(results)
+        result_buffer[n*iterations:(n+1)*iterations] = np.array(results)
         buffers_merged.value += 1
 
 
-def acquire_avg_data_parallel(worker, proc_fun, result_shape):
+def acquire_avg_data_parallel(worker, proc_fun, result_shape, plot=True):
     """
     :param worker: Function which the subordinate threads execute as their target
     :param proc_fun: Function used by the subordinate threads to process their buffers
@@ -100,7 +127,7 @@ def acquire_avg_data_parallel(worker, proc_fun, result_shape):
     config={'clock_edge': 'rising', 'clock_source': 'reference',  
             'trigger_coupling': 'DC',  'trigger_operation': 'or', 
             'trigger_source2': 'disabled','trigger_level2': 1.0, 'trigger_edge2': 'rising', 
-            'trigger_level1': 0.6, 'trigger_edge1': 'rising', 'trigger_source1': 'external', 
+            'trigger_level1': 0.1, 'trigger_edge1': 'rising', 'trigger_source1': 'external', 
             'ch1_enabled': True,  'ch1_coupling': 'AC', 'ch1_range': 1, 'ch1_filter': False, 
             'ch2_enabled': False, 'ch2_coupling': 'AC','ch2_range': 1, 'ch2_filter': False,            
             'bufferCount': buffer_count,'recordsPerBuffer': records_per_buffer, 'trigger_delay': 0, 'timeout': timeout,
@@ -135,12 +162,18 @@ def acquire_avg_data_parallel(worker, proc_fun, result_shape):
         for w in workers:
             w.start()
         time.sleep(1)
+        
+        import atexit
+        atexit.register(lambda: [w.terminate() for w in workers])
+
 
         # Initialize things used during capture
-        plotter = ScriptPlotter()
+        if plot:
+            plotter = ScriptPlotter()
+            plotter.init_plot('Data', rank=1, accum=False)
         buffers_acquired, buffers_completed, plot_count = 0, 0, 0
         start_time = time.time()
-        plotter.init_plot('Data', rank=1, accum=False)
+        
 
         # Begin capture
         ret = Az.AlazarStartCapture(handle)
@@ -176,21 +209,25 @@ def acquire_avg_data_parallel(worker, proc_fun, result_shape):
             buf_ready_events[buf_idx].set()
             
             # If a second has elapsed, replot the avg_buffer
-            if (time.time() - start_time) / seconds_per_plot > plot_count:
-                with avg_buffer.get_lock():
-                    plotter.msg(buffers_acquired, bufs_merged.value)
-                    plotter.plot(np.frombuffer(avg_buffer.get_obj()), 'Data')
-                plot_count += 1
+            if plot:
+                if (time.time() - start_time) / seconds_per_plot > plot_count:
+                    with avg_buffer.get_lock():
+                        plotter.msg(buffers_acquired, bufs_merged.value)
+                        plotter.plot(np.frombuffer(avg_buffer.get_obj()), 'Data')
+                    plot_count += 1
     finally:
         Az.AlazarAbortAsyncRead(handle)
         final_time = time.time()
         print 'Unready Count', unready_count
         total_time = final_time - start_time
         print 'Total time', total_time
-        actual_time_per_buffer = total_time / buffers_completed
-        print 'Time per buffer %.2e' %  actual_time_per_buffer
-        errf = lambda a, b: abs(a - b)/min(a, b)
-        print 'Perceived overhead %.1f%%' % (errf(actual_time_per_buffer, seconds_per_buffer) * 100)
+        if buffers_completed:
+            actual_time_per_buffer = total_time / buffers_completed
+            print 'Time per buffer %.2e' %  actual_time_per_buffer
+            errf = lambda a, b: abs(a - b)/min(a, b)
+            print 'Perceived overhead %.1f%%' % (errf(actual_time_per_buffer, seconds_per_buffer) * 100)
+        else:
+            print 'No buffers completed'
 
     return np.frombuffer(avg_buffer.get_obj())
 
@@ -200,5 +237,5 @@ def identity(x):
 
 if __name__ == "__main__":
     #import cProfile
-    acquire_avg_data_parallel(average_worker, identity, (records_per_buffer,))
+    acquire_avg_data_parallel(average_worker, identity, (records_per_buffer,), plot=True)
     print 'done'
