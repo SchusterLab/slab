5bf069b0df18a97d93023bb68c09502398e03c97
ge <ge@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-05-16 21:40:26 +0000

    HMAC_KEY, bug in SlabFileRemote filename parameter

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -19,6 +19,7 @@ manager_host = 'localhost'
 manager_port = 5555
 manager_uri = "PYRO:%s@%s:%d" % (manager_id, manager_host, manager_port)
 RUNNING = True
+Pyro4.config.HMAC_KEY = '6551d449b0564585a9d39c0bd327dcf1'
 
 h5file_directory = r'S:\_Data'
 h5file_filter = 'HDF5 Files (*.h5)'
@@ -718,7 +719,7 @@ class SlabFileRemote:
         else:
             self.manager = manager
         self.filename = filename
-        if context == () and filename is None:
+        if context == () and filename is not None:
             self.context = (filename,)
         else:
             self.context = context
