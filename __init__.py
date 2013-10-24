b03cb15f44b0c1b4ab45da28fc1282fe088c8c8f
ge <ge@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-10-24 11:27:09 +0000

    added the matplotlib_text_wrapper.py code to prettify plots Ge

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from datamanagement import SlabFile, h5File
 from plotting import ScriptPlotter
 from dsfit import set_fit_plotting,argselectdomain,selectdomain,zipsort,fitgeneral, fitexp, fitgauss, fithanger, fithangertilt, fitlor, fitdecaysin,fithanger_new,hangerfunc_new
 from dataanalysis import *
+from matplotlib_text_wrapper import *
