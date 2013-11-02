34a9c98bbe94692baac6c3d22a3c17b5109ba617
dave <dave@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2013-11-02 17:32:59 +0000

    Added a few warnings related to importing slab on OSX

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 from instruments import InstrumentManager
 from datamanagement import SlabFile, h5File
-from plotting import ScriptPlotter
+try:
+    from plotting import ScriptPlotter
+except:
+    "Warning could not import ScriptPlotter"
 from dsfit import set_fit_plotting,argselectdomain,selectdomain,zipsort,fitgeneral, fitexp, fitgauss, fithanger, fithangertilt, fitlor, fitdecaysin,fithanger_new,hangerfunc_new
 from dataanalysis import *
 from matplotlib_text_wrapper import *
