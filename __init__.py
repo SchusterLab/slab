bb814a81190911de4443b50a32b086bdd64447f4
ge <ge@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2014-08-27 00:00:52 +0000

    Fixed Tek5014 bug added experiment.py

--- __init__.py
+++ __init__.py
@@ -7,3 +7,4 @@ except:
 from dsfit import set_fit_plotting,argselectdomain,selectdomain,zipsort,fitgeneral, fitexp, fitgauss, fithanger, fithangertilt, fitlor, fitdecaysin,fithanger_new,hangerfunc_new
 from dataanalysis import *
 from matplotlib_text_wrapper import *
+from experiment import Experiment
