27876f0852501a915f4c9e4dc2eea386ede49ec3
ge <ge@84ce8a1a-afed-4f05-8c8e-f9845bb8233b>
2015-04-30 21:35:15 +0000

    Added DigitalAttenuator package and class.  --DS

---
+++ instruments/DigitalAttenuator.py
@@ -0,0 +1,44 @@
+__author__ = 'dave'
+
+from slab.instruments import SerialInstrument
+from math import floor
+import time
+
+
+class DigitalAttenuator(SerialInstrument):
+
+
+    """This is a driver for the Arduino Uno based digital attenuator
+       It is based on a HITITE HMC424 digital attenuator.  It also uses a custom board to invert the
+       arduino logic.
+    """
+
+
+def __init__(self, name="", address='COM6', enabled=True, timeout=0):
+    """Note when initialized the attenuator board will reset to -31.5dB"""
+    SerialInstrument.__init__(self, name, address, enabled, timeout, querysleep=0.1)
+    self.term_char = '\n'
+
+
+def get_id(self):
+    """Gets the id string of the instrument"""
+    self.ser.flushInput()
+    self.id = self.query('I')
+    self.id = self.query('I')
+    return self.id
+
+
+def set_attenuator(self, atten):
+    """Sets the attenuation from -31.5 to 0dB
+       can be set in 0.5 dB increments, if not multiple of 0.5 it rounds up
+       ignores the sign of the attenuation"""
+    val = floor(abs(atten) / 0.5)
+    self.write('S%d' % val)
+
+
+def get_attenuator(self):
+    """Returns current attenuation setting of the digital attenuator in dB"""
+    self.ser.flushInput()
+    val = float(self.query('G'))
+    return -val * 0.5
+
