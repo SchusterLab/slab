d98ea9973b7e31675995a6b770af06272fd03e2f
luy100 <luy100@uchicago.edu>
2016-11-07 17:03:06 -0600

    enabled pi calibration for the fast arb(Nelson)

--- experiments/ExpLib/QubitPulseSequenceExperiment.py
+++ experiments/ExpLib/QubitPulseSequenceExperiment.py
@@ -171,18 +171,25 @@ class QubitPulseSequenceExperiment(Experiment):
             testing_redpitaya = False
 
             if not testing_redpitaya:
-                expt_avg_data = setup_redpitaya_adc(m8195a,num_experiments=num_experiments,window=self.cfg['readout']['window'],shots=self.cfg[self.expt_cfg_name]['averages'],plot_data=self.cfg['readout']['plot'])
+                expt_avg_data_raw = setup_redpitaya_adc(m8195a,num_experiments=num_experiments,window=self.cfg['readout']['window'],shots=self.cfg[self.expt_cfg_name]['averages'],plot_data=self.cfg['readout']['plot'])
 
             ## This is for testing the redpitaya behaviour
             if testing_redpitaya:
                 correct_counter = 0
                 repeat = 100
                 for ii in range(repeat):
-                    expt_avg_data = setup_redpitaya_adc(m8195a,num_experiments=num_experiments,window=self.cfg['readout']['window'],shots=self.cfg[self.expt_cfg_name]['averages'],plot_data=False)
-                    if ((expt_avg_data[1:]-expt_avg_data[:-1])>0).all():
+                    expt_avg_data_raw = setup_redpitaya_adc(m8195a,num_experiments=num_experiments,window=self.cfg['readout']['window'],shots=self.cfg[self.expt_cfg_name]['averages'],plot_data=False)
+                    if ((expt_avg_data_raw[1:]-expt_avg_data_raw[:-1])>0).all():
                         correct_counter += 1
 
                 print "correct percent: " + str(correct_counter/float(repeat))
+
+            if self.cfg[self.expt_cfg_name]['use_pi_calibration']:
+                zero_amp = expt_avg_data_raw[-2]
+                pi_amp = expt_avg_data_raw[-1]
+                expt_avg_data = (expt_avg_data_raw[:-2]-zero_amp)/(pi_amp - zero_amp)
+            else:
+                expt_avg_data = expt_avg_data_raw
 
             if self.data_file != None:
                 self.slab_file = SlabFile(self.data_file)
