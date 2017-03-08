51a0b593daddba9c979319211c2935ca57e8d73d
Dave Schuster <david.schuster@gmail.com>
2017-03-08 15:50:55 -0600

    update sequence

--- experiments/General/PulseSequences/SingleQubitPulseSequences.py
+++ experiments/General/PulseSequences/SingleQubitPulseSequences.py
@@ -23,8 +23,8 @@ class RabiSequence(QubitPulseSequence):
         if self.expt_cfg['sweep_amp']:
             self.psb.append('q','general', self.pulse_type, amp=pt, length=self.expt_cfg['length'],freq=self.expt_cfg['iq_freq'],phase=self.expt_cfg['phase'])
         else:
-            self.psb.append('q','general', self.pulse_type, amp=self.expt_cfg['a'], length=pt,freq=self.expt_cfg['iq_freq'],phase=self.expt_cfg['phase'])
-            # self.psb.append('q2', 'general', self.pulse_type, amp=self.expt_cfg['a'], length=pt,freq=self.expt_cfg['iq_freq'], phase=self.expt_cfg['phase'])
+            # self.psb.append('q','general', self.pulse_type, amp=self.expt_cfg['a'], length=pt,freq=self.expt_cfg['iq_freq'],phase=self.expt_cfg['phase'])
+            self.psb.append('q2', 'general', self.pulse_type, amp=self.expt_cfg['a'], length=pt,freq=self.expt_cfg['iq_freq'], phase=self.expt_cfg['phase'])
 
 
 
