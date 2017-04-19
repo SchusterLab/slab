587203cb3289b2fcfa367e25d60a8e72dffcbb0a
Dave Schuster <david.schuster@gmail.com>
2017-04-19 09:59:09 -0500

    add **kwargs to EFRabiExperiment

--- experiments/General/SingleQubitPulseSequenceExperiment.py
+++ experiments/General/SingleQubitPulseSequenceExperiment.py
@@ -171,9 +171,9 @@ class SpinEchoExperiment(QubitPulseSequenceExperiment):
 
 class EFRabiExperiment(QubitPulseSequenceExperiment):
     def __init__(self, path='', prefix='EF_Rabi', config_file='..\\config.json', **kwargs):
-        QubitPulseSequenceExperiment.__init__(self, path=path, prefix=prefix, config_file=config_file, liveplot_enabled=False,
+        QubitPulseSequenceExperiment.__init__(self, path=path, prefix=prefix, config_file=config_file,
                                               PulseSequence=EFRabiSequence, pre_run=self.pre_run,
-                                              post_run=self.post_run)
+                                              post_run=self.post_run, **kwargs)
 
     def pre_run(self):
         self.drive.set_frequency(
