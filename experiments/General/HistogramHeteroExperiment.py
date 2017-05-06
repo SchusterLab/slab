7e65ca9dec38cf7aae70f38fbc5ce10a6de9308c
Dave Schuster <david.schuster@gmail.com>
2017-05-06 12:04:11 -0500

    save attenpts for plotting in HistogramHetero

--- experiments/General/HistogramHeteroExperiment.py
+++ experiments/General/HistogramHeteroExperiment.py
@@ -177,6 +177,7 @@ class HistogramHeteroExperiment(Experiment):
 
             with self.datafile() as f:
                 f.append_pt('atten', atten)
+                f.add('attenpts', attenpts)
                 f.append_line('freq', freqpts)
                 f.append_line('max_contrast_data_ch1', max_contrast_data[0, :])
                 f.append_line('max_contrast_data_ch2', max_contrast_data[1, :])
