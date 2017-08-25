2c377228f2206f0a4064cccb70b76735cef2d0ee
Alex Ma <maruichao@gmail.com>
2017-08-25 15:24:36 -0500

    remove pxdac1 in localinstruments (switched to fast awg)

--- instruments/localinstruments.py
+++ instruments/localinstruments.py
@@ -1,8 +1,11 @@
 from slab.instruments.awg.PXDAC4800 import PXDAC4800
 
 class LocalInstruments():
+
+    # need a classmethod to generate these class variables inside init?
+
     inst_dict = {}
-    inst_dict['pxdac4800_1'] = PXDAC4800(1)
+    # inst_dict['pxdac4800_1'] = PXDAC4800(1)
     inst_dict['pxdac4800_2'] = PXDAC4800(2)
     inst_dict['pxdac4800_3'] = PXDAC4800(3)
 
