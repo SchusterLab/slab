dd79a8e5724f70588bc411e3e72ba2c7aa680471
Dave Schuster <david.schuster@gmail.com>
2017-11-08 12:17:08 -0600

    merge awgpulses

--- experiments/ExpLib/awgpulses.py
+++ experiments/ExpLib/awgpulses.py
@@ -1,25 +1,35 @@
 __author__ = 'dave'
 
 import numpy as np
+import numexpr as ne
 
 
-def sideband(t, plus, minus, freq=0, phase=0, offset=False, offset_fit_lin=0,offset_fit_quad=0,t0=0):
-    if offset:
+def sideband(t, plus, minus, freq=0, phase=0, offset=False, offset_fit_lin=0,offset_fit_quad=0, origin=0):
+    if freq==0 and phase==0:
+        return (plus - minus), np.zeros(t.shape)
+
+    elif offset:
         if (not max(plus) == 0):
             time_step = t[1]-t[0]
             freq_calibrated = getFreq(plus,freq,offset_fit_lin,offset_fit_quad);
             freq_integ_array = np.cumsum(freq_calibrated)*time_step
             # np.savetxt('time.out', t, delimiter=',')
         return ( np.cos(2 * np.pi * (freq_integ_array/1.0e9) + phase*np.pi/180.0) * plus - np.cos(2 * np.pi * (freq_integ_array/1.0e9) + phase*np.pi/180.0) * minus,
-             +np.sin(2 * np.pi * (freq_integ_array/1.0e9)+ phase*np.pi/180.0) * plus + np.sin(2 * np.pi * (freq_integ_array/1.0e9) + phase*np.pi/180.0) * minus)
+             -np.sin(2 * np.pi * (freq_integ_array/1.0e9)+ phase*np.pi/180.0) * plus - np.sin(2 * np.pi * (freq_integ_array/1.0e9) + phase*np.pi/180.0) * minus)
     else:
         # For ML0218 Mixer
-        return ( np.cos(2 * np.pi * (freq/1.0e9 * (t-t0))+ phase*np.pi/180.0) * plus - np.cos(2 * np.pi * (freq/1.0e9 * (t-t0)) + phase*np.pi/180.0) * minus,
-             +np.sin(2 * np.pi * (freq/1.0e9 * (t-t0))+ phase*np.pi/180.0) * plus +np.sin(2 * np.pi * (freq/1.0e9 * (t-t0)) + phase*np.pi/180.0) * minus)
+        # return ( np.cos(2 * np.pi * (freq/1.0e9 * t)+ phase*np.pi/180.0) * plus - np.cos(2 * np.pi * (freq/1.0e9 * t) + phase*np.pi/180.0) * minus,
+        #      +np.sin(2 * np.pi * (freq/1.0e9 * t)+ phase*np.pi/180.0) * plus +np.sin(2 * np.pi * (freq/1.0e9 * t) + phase*np.pi/180.0) * minus)
         #
         # For IQ0317 Mixer
         # return ( np.cos(2 * np.pi * (freq/1.0e9 * t)+ phase*np.pi/180.0) * plus - np.cos(2 * np.pi * (freq/1.0e9 * t) + phase*np.pi/180.0) * minus,
         #      -np.sin(2 * np.pi * (freq/1.0e9 * t)+ phase*np.pi/180.0) * plus - np.sin(2 * np.pi * (freq/1.0e9 * t) + phase*np.pi/180.0) * minus)
+
+        wts = ne.evaluate('2 * (freq/1.0e9 * (t-origin))+ phase/180.0') * np.pi
+        cosdata = ne.evaluate('cos(wts)')
+        sindata = ne.evaluate('sin(wts)')
+        # return ne.evaluate('cosdata * (plus - minus)'), ne.evaluate('- sindata * (plus + minus)')
+        return ne.evaluate('cosdata * (plus - minus)'), ne.evaluate('- sindata * (plus + minus)')
 
 
 
@@ -42,30 +52,76 @@ def getFreq(pulse,freq,offset_fit_lin=0,offset_fit_quad=0):
 
 
 def gauss(t, a, t0, sigma):
-    if sigma >0:
-        return (t >= t0-2*sigma) * (t <= t0+2*sigma)*a * np.exp(-1.0 * (t - t0) ** 2 / (2 * sigma ** 2))
-    else:
-        return 0*(t-t0)
+    vv = np.zeros(t.shape)
+    if sigma > 0:
+        start = np.searchsorted(t, t0 - 2 * sigma, side='left')
+        stop = np.searchsorted(t, t0 + 2 * sigma, side='right') # include both ends <=/<=
+        t = t[start:stop]
+        vv[start:stop] = a * np.exp(-1.0 * (t - t0) ** 2 / (2 * sigma ** 2))
+    return vv
 
+def square(t, a, t0, w, sigma=0):
+    vv = np.zeros(t.shape)
+    if w > 0:
+        start = np.searchsorted(t, t0 - 2 * sigma, side='left')
+        stop = np.searchsorted(t, t0 + w + 2 * sigma, side='right') # include both ends <=/<=
+        t = t[start:stop]
+        if sigma > 0:
+            vv[start:stop] = a * (
+                (t >= t0) * (t < t0 + w) +  # Normal square pulse
+                (t >= t0 - 2 * sigma) * (t < t0) * np.exp(-(t - t0) ** 2 / (2 * sigma ** 2)) +  # leading gaussian edge
+                (t >= t0 + w) * (t <= t0 + w + 2 * sigma) * np.exp(-(t - (t0 + w)) ** 2 / (2 * sigma ** 2)) # trailing edge
+                )
+        else:
+            vv[start:stop] = a * (t >= t0) * (t < t0 + w)
+    return vv
+
+def linear_ramp(t, start_a, stop_a, t0, w):
+    vv = np.zeros(t.shape)
+    if w > 0:
+        start = np.searchsorted(t, t0, side='left')
+        stop = np.searchsorted(t, t0 + w, side='left') # include only left end <=/<
+        t = t[start:stop]
+        vv[start:stop] = ( (stop_a - start_a) * (t - t0) / w + start_a)
+    return vv
+
+def linear_ramp_with_mod(t, start_a, stop_a, t0, w, mod_amp, mod_freq, mod_start_phase):
+    vv = np.zeros(t.shape)
+    if w > 0:
+        start = np.searchsorted(t, t0, side='left')
+        stop = np.searchsorted(t, t0 + w, side='left') # include only left end <=/<
+        t = t[start:stop]
+        vv[start:stop] = ( (stop_a - start_a) * (t - t0) / w + start_a +
+                           mod_amp * np.sin( 2*np.pi*(mod_freq/1.0e9)*(t-t0) + mod_start_phase/180.0 )
+                           )
+    return vv
+
+def logistic_ramp(t, start_a, stop_a, t0, w):
+    # smooth S-shaped ramp using logistic function
+    # truncated and rescaled to be continuous
+    # bigger r - steeper slope
+    vv = np.zeros(t.shape)
+    if w > 0:
+        start = np.searchsorted(t, t0, side='left')
+        stop = np.searchsorted(t, t0 + w, side='left') # include only left end <=/<
+        t = t[start:stop]
+        r = 8.0 / w
+        scale = (1 + np.exp(r * w / 2.0)) / (-1 + np.exp(r * w / 2.0))
+        vv[start:stop] = (((start_a + stop_a * np.exp(r * (t - (t0 + w / 2.0)) ))
+                           / (1 + np.exp(r * (t - (t0 + w / 2.0)) )) - (start_a + stop_a) / 2.0)
+                            * scale + (start_a + stop_a) / 2.0)
+    return vv
+
+
+
+####
+## the following are old (unused?) pulses:
 
 def dgauss(t, a, t0, sigma):
     return a * np.exp(-1.0 * (t - t0) ** 2 / (2 * sigma ** 2)) * (t - t0) / sigma ** 2
 
-
 def ramp(t, a, t0, w):
     return a * (t - t0) * (t >= t0) * (t < t0 + w)
-
-
-def square(t, a, t0, w, sigma=0):
-    if sigma>0:
-        return a * (
-            (t >= t0) * (t < t0 + w) +  # Normal square pulse
-            (t >= t0-2*sigma) * (t < t0) * np.exp(-(t - t0) ** 2 / (2 * sigma ** 2)) +  # leading gaussian edge
-            (t >= t0 + w)* (t <= t0+w+2*sigma) * np.exp(-(t - (t0 + w)) ** 2 / (2 * sigma ** 2))  # trailing edge
-        )
-    else:
-        return a * (t >= t0) * (t < t0 + w)
-
 
 def trapezoid(t, a, t0, w, edge_time=0):
     return a * (
@@ -75,11 +131,30 @@ def trapezoid(t, a, t0, w, edge_time=0):
             t >= t0 + w + 2 * edge_time) )
 
 
-def get_pulse_span_length(cfg, type, length, ramp_sigma = None):
-    if type == "gauss":
-        return length * 4 +cfg['spacing'] ## 4 sigma
-    if type == "square":
-        if ramp_sigma == None:
+def get_pulse_span_length(cfg, type, length):
+
+    if length > 0:
+
+        if type == "gauss":
+            return length * 4 + cfg['spacing'] ## 4 sigma + spacing
+        if type == "square" or type == "square_exp":
             return length + 4 * cfg[type]['ramp_sigma'] +cfg['spacing']
-        else:
-            return length + 4 * ramp_sigma +cfg['spacing']
+
+        if type in ["ramp", "linear_ramp", "linear_ramp_with_mod", "logistic_ramp"]:
+            return length
+    else:
+        return 0.0
+
+def get_pulse_area(type=None, length=0, a=0, start_a=0, stop_a=0):
+
+    if type in ["linear_ramp", "linear_ramp_with_mod", 'logistic_ramp']:
+        return (start_a + stop_a)/2.0*length
+    else:
+        return 0.0
+
+def get_pulse_power(type=None, length=0, a=0, start_a=0, stop_a=0):
+
+    if type in ["linear_ramp", "linear_ramp_with_mod", 'logistic_ramp']:
+        return (((start_a + stop_a)/2.0)**2)*length
+    else:
+        return 0.0
