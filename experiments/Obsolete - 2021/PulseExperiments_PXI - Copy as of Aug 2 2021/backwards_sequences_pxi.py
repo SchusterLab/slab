dc5b585380141375ad71bd0b6bb6098291dcaa9f
Dave Schuster <circuitqed@users.noreply.github.com>
2021-08-06 10:22:36 -0500

    cleaning things up

--- experiments/Obsolete - 2021/PulseExperiments_PXI - Copy as of Aug 2 2021/backwards_sequences_pxi.py
+++
@@ -1,3 +0,0 @@
-
-''' Sequencer and pulse classes have been written for backwards pulses such that you must write the actual
-experiment sequences backwards in order to sync pulse chains to readout rather than time zero '''
